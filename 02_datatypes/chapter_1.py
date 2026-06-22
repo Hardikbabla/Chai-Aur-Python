0957b20a25b47362826004255dce728cb865ef95
Hardik Babla <hardikbabla.dev@gmail.com>
2026-06-22 20:59:59 +0530

    Mutable & Immutable Objects

---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,8 @@
+sugar_amount = 2
+print(f"first sugar : {sugar_amount}")
+
+sugar_amount = 3
+print(f"second sugar : {sugar_amount}")
+
+print(f"ID of 2 : {id(2)}")
+print(f"ID of 2 : {id(3)}")
---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,8 @@
+spice_mix = set()
+print(f"Initial spice_mix : {(spice_mix)}")
+print(f"Initial spice_mix ID : {id(spice_mix)}")
+spice_mix.add("Ginger")
+spice_mix.add("Cardamom")
+spice_mix.add("Lemon")
+print(f"After spice_mix : {(spice_mix)}")
+print(f"After spice_mix ID : {id(spice_mix)}")
