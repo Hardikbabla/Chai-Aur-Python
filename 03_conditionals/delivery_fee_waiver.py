fb2cfcebb04b2dc2d883e52385aee5abaa381a1b
Hardik Babla <hardikbabla.dev@gmail.com>
2026-06-26 14:49:26 +0530

    Delivery Fees Waiver System

---
+++ 03_conditionals/delivery_fee_waiver.py
@@ -0,0 +1,17 @@
+# Delivery Fees Waiver System
+# =====================================================================
+"""
+You run an online tea store.
+If the order amount is more than ₹300, delivery is free,
+otherwise, it cost ₹30
+Task:
+    - Input: order_amount
+    - Use ternary operator to decide delivery fee
+"""
+# =====================================================================
+
+order_amount = int(input("Enter the order amount: "))
+
+delivery_fee = 0 if order_amount > 300 else 30
+print(f"Delivery fees is : {delivery_fee}")
+
