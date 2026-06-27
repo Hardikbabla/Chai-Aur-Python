8f10421ae07082a670b5cb5f6751a5e6da6197b1
Hardik Babla <hardikbabla.dev@gmail.com>
2026-06-27 07:58:59 +0530

    Batch Chai Preparation

---
+++ 04_loops/02_batch_chai.py
@@ -0,0 +1,13 @@
+# Batch Chai Preparation
+# =====================================================================
+"""
+A chai shop makes tea in batches ever 15 minutes.
+You want to stimulate 4 batches.
+Task:
+    - Use range() to stimulate batch numbers.
+    - Print: "Preparing chai for batch #{number}"
+"""
+# =====================================================================
+
+for batch in range(1, 5):
+    print(f"Preparing chai for batch #{batch}")
