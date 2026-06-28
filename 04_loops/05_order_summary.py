1be4e410d481eefd383d0b94583ee8bc891c6040
Hardik Babla <hardikbabla.dev@gmail.com>
2026-06-28 08:37:07 +0530

    Zip Can Combine Lists

---
+++ 04_loops/05_order_summary.py
@@ -0,0 +1,15 @@
+# Zip Can Combine Lists
+# =====================================================================
+"""
+You're preparing an order summary with customer names and their total bill.
+Task:
+    - Use two lists: one for names and one for bills.
+    - Print: "{Name} paid ₹{amount}"
+"""
+# =====================================================================
+
+names = ["Hitesh", "Meera", "Sam", "Ali"]
+bills = [50, 70, 100, 55]
+
+for name, amount in zip(names, bills):
+    print(f"{name} paid ₹{amount}")
