df436bd794934e41e16419924a70a1429ab29ad1
Hardik Babla <hardikbabla.dev@gmail.com>
2026-06-28 08:05:25 +0530

    Looping through list - Orders name

---
+++ 04_loops/03_tea_orders.py
@@ -0,0 +1,15 @@
+# Looping through list - Orders name
+# =====================================================================
+"""
+You receive a list of names for chai orders.
+The goal is to print put order queue.
+Task:
+    - Use a list of names.
+    - Print: "Order ready for {name}"
+"""
+# =====================================================================
+
+orders = ["Hitesh", "Aman", "Becky", "Carlos"]
+
+for name in orders:
+    print(f"Order ready for {name}")
